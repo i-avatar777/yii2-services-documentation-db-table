e12daf9d189eee969493c809961159fc83874188
Святослав <dram1008@yandex.ru>
2021-10-04 17:40:55 +0300

    0.0.4

--- src/DbTable.php
+++ src/DbTable.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace common\services\documentation;
+namespace iAvatar777\services\DbTable;
 
 use common\models\information_schema\InnoDbColumn;
 use common\models\information_schema\InnoDbTable;
